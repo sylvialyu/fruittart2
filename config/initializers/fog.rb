036b20fe29d296fc9e8ddfebe3747e6965ce4960
nn70 <nn4work@gmail.com>
2017-05-21 23:22:56 +0800

    fog setting

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -5,3 +5,4 @@ google_client_email: 'nn70nn70@developer.gserviceaccount.com',
 google_key_location: Rails.root.join('private','google-cloud-service-key.p12'),
 google_storage_access_key_id: ENV["GOOGLE_ID"],
 google_storage_secret_access_key: ENV["GOOGLE_KEY"]
+)
