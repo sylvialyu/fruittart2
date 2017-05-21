5fe3c54433853430ef5c92d56a1b81c558f3345f
nn70 <nn4work@gmail.com>
2017-05-21 23:16:33 +0800

    fog setting

---
+++ config/initializers/fog.rb
@@ -0,0 +1,7 @@
+GoogleStorage = Fog::Storage.new(
+provider: 'Google',
+google_project: 'fullstack-tim',
+google_client_email: 'nn70nn70@developer.gserviceaccount.com',
+google_key_location: Rails.root.join('private','google-cloud-service-key.p12'),
+google_storage_access_key_id: ENV["GOOGLE_ID"],
+google_storage_secret_access_key: ENV["GOOGLE_KEY"]
