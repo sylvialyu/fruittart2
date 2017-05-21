a55c6b0e519a41df9c47cc832e61bcbc4146a139
nn70 <nn4work@gmail.com>
2017-05-21 23:13:36 +0800

    fog setting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
   google_project: 'fullstack-tim',
   google_client_email: 'nn70nn70@developer.gserviceaccount.com',
   google_key_location: Rails.root.join('private','google-cloud-service-key.p12'),
-  google_storage_secret_access_key_id: ENV["GOOGLE_ID"],
+  google_storage_access_key_id: ENV["GOOGLE_ID"],
   google_storage_secret_access_key: ENV["GOOGLE_KEY"]
 )
 
