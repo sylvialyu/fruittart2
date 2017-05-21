1d6f23276eca2d6a000487883de1d33e79b72b6b
nn70 <nn4work@gmail.com>
2017-05-21 23:56:00 +0800

    fog setting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,19 @@
-require 'carrierwave'
-
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/google'                        # required
-  config.fog_credentials = {
-    provider:                         'Google',
-    google_storage_access_key_id:     ENV['GOOGLE_STORAGE_ACCESS_KEY_ID'],
-    google_storage_secret_access_key: ENV['GOOGLE_STORAGE_SECRET_ACCESS_KEY']
-  }
-  config.fog_directory = ENV['GOOGLE_STORAGE_BUCKET_NAME']
+  if Rails.env.production?
+    config.fog_provider = 'fog'
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV["GOOG_ACCESS_KEY_ID"],
+
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+
+      region:                ENV["AWS_REGION"]
+
+    }
+    config.fog_directory  = ENV["AWS_BUCKET_NAME"]
+
+
+  else
+    config.storage :file
+  end
 end
