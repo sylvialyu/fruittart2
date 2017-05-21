55c3b9351d1924c45dfaf5c8959d2ac788130011
nn70 <nn4work@gmail.com>
2017-05-21 23:00:16 +0800

    fog setting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,9 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog'
     config.fog_credentials = {
       provider:              'Google',
-      goog_access_key_id:     ENV["GOOG_ACCESS_KEY_ID"],
+      goog_storage_access_key_id:     ENV["GOOG_ACCESS_KEY_ID"],
 
-      goog_secret_access_key: ENV["GOOG_SECRET_ACCESS_KEY"],
+      goog_storage_secret_access_key: ENV["GOOG_SECRET_ACCESS_KEY"],
 
       region:                ENV["GOOG_REGION"]
 
