5c4df1ab163b811ce2c474a157e98c15645e4fc0
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-05-27 18:50:46 +0800

    general beautification on products#index

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -29,7 +29,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
-  process resize_to_fill: [800, 800]
+  process resize_to_fill: [738, 426]
 
  version :thumb do
    process resize_to_fill: [200,200]
