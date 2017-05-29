63db08f6f77fac3ba4e9bb79f53de541ea2e8081
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-05-29 10:34:45 +0800

    new changes from github origin dev

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170528164530) do
-
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
